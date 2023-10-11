57962c3694aa40b3407869cf3c863f99c3fc75b9
Laxma1996 <lakshmakandula@gmail.com>
2023-10-10 19:05:36 -0500

    First draft of the application using MVC .Net framework

---
+++ Laxma_WebResume/Laxma_WebResume/Controllers/ExperienceController.cs
@@ -0,0 +1,28 @@
+﻿using Laxma_WebResume.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Laxma_WebResume.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        public IActionResult Education()
+        {
+            return View("Education");
+        }
+        public IActionResult TCS()
+        {
+            return View("TCS");
+        }
+
+        public IActionResult Infosys()
+        {
+            return View("Infosys");
+        }
+
+        public IActionResult SIUC()
+        {
+            return View("SIUC");
+        }
+    }
+}
---
+++ Laxma_WebResume/Laxma_WebResume/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laxma_WebResume.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
